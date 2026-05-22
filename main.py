756b133c69aa95c97325567723b3b495dc0dd7f4
ZeroMeOut <wiziset@gmail.com>
2026-05-22 23:06:54 +0100

    Gemma 3 kinda sucks lmao

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ def main():
     game = TicTacToe()
     game.push(0, 0)
     game.push(2, 0) 
+    game.push(1, 1)
+    game.push(2, 1)
+    game.push(1, 2)
     print("Move and score: ", minimax(game, 10, True, float('-inf'), float('inf')))
     game.display_board()
+    print(game.display_board_as_list())
 
 if __name__ == "__main__":
     main()
